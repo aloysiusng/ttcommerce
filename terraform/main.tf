a28f3e5f4b600d2c92bbaddc264ce4ef354d1996
Aloysius <aloysiusng5@gmail.com>
2023-09-06 03:49:26 +0800

    update iam  + remove acl for S3

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,10 +3,6 @@
 resource "aws_s3_bucket" "images_bucket" {
   bucket = var.images_bucket_name
 }
-resource "aws_s3_bucket_acl" "images_bucket_acl" {
-  bucket = var.images_bucket_name
-  acl    = "private"
-}
 
 resource "aws_s3_bucket_public_access_block" "images_bucket_public_access_block" {
   bucket                  = var.images_bucket_name
@@ -22,11 +18,6 @@ output "images_bucket_arn" {
 # lambda bucket
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = var.lambda_bucket_name
-}
-
-resource "aws_s3_bucket_acl" "lambda_bucket_acl" {
-  bucket = var.lambda_bucket_name
-  acl    = "private"
 }
 
 resource "aws_s3_bucket_public_access_block" "lambda_bucket_public_access_block" {
