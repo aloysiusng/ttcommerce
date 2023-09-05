d1d54351d4eb0ec15e7a905096052a772cc524e5
Aloysius <aloysiusng5@gmail.com>
2023-09-06 01:06:20 +0800

    s3 variables at root

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "images_bucket" {
-  bucket = "images_bucket"
+  bucket = var.images_bucket
+  acl    = "private"
+}
+resource "aws_s3_bucket_acl" "images_bucket_acl" {
+  bucket = var.images_bucket
   acl    = "private"
 }
