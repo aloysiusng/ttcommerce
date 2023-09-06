faa5e66f16fb2a4ed40bff487a42f1c1f25c3fd4
DESKTOP-RLI54N2\Yu_Hao <yuhaochua99@gmail.com>
2023-09-06 23:34:41 +0800

    add create_review endpoint

--- terraform/main.tf
+++ terraform/main.tf
@@ -338,3 +338,38 @@ resource "aws_lambda_permission" "delete_product_permission" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
+
+# ========================= POST /create_review ========================================
+resource "aws_lambda_function" "create_review" {
+  function_name    = "create_review"
+  filename         = "../backend/create_review.zip"
+  role             = aws_iam_role.super_lambda_role.arn
+  handler          = "create_review.create_review.lambda_handler"
+  #                   function name
+  source_code_hash = filebase64sha256("../backend/create_review.zip")
+
+  runtime = "python3.8"
+  timeout = 900
+}
+resource "aws_cloudwatch_log_group" "create_review" {
+  name              = "/aws/lambda/${aws_lambda_function.create_review.function_name}"
+  retention_in_days = 30
+}
+resource "aws_apigatewayv2_integration" "create_review_integration" {
+  api_id             = aws_apigatewayv2_api.lambda.id
+  integration_uri    = aws_lambda_function.create_review.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+resource "aws_apigatewayv2_route" "create_review_route" {
+  api_id    = aws_apigatewayv2_api.lambda.id
+  route_key = "POST /create_review"
+  target    = "integrations/${aws_apigatewayv2_integration.create_review_integration.id}"
+}
+resource "aws_lambda_permission" "create_review_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.create_review.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+}
