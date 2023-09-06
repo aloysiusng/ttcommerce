6d6635ec924af81a397126a243355b2468f8abd9
Cleve Huang <hskcleve@gmail.com>
2023-09-06 14:49:43 +0800

    caa 6 sep

--- frontend/tt-hackathon-fe/pages/supplier-management.js
+++ frontend/tt-hackathon-fe/pages/supplier-management.js
@@ -1,12 +1,47 @@
 import Head from "next/head";
-import styles from "../styles/SupplierAnalytics.module.css";
-import { useContext, useState } from "react";
+import styles from "../styles/SupplierManagement.module.css";
+import { useContext, useState, useEffect } from "react";
 import Sidebar from "../components/sidebar";
 import Navbar from "../components/Navbar";
 import { UserContext } from "./_app";
+import {
+  getAllAffiliatesBySellerId,
+  getAllProductsBySellerId,
+} from "../utils/seller-service";
+import ProductCard from "../components/ProductCard";
+import UserMiniCard from "../components/UserMiniCard";
 
 export default function SupplierManagement() {
   const { user, setUser } = useContext(UserContext);
+  const [sellerAllProducts, setSellerAllProducts] = useState([]);
+  const [sellerAllAffiliates, setSellerAllAffiliates] = useState([]);
+
+  async function fetchSellerAllProducts() {
+    try {
+      const products = await getAllProductsBySellerId(user.email);
+      setSellerAllProducts(products);
+      console.log(products);
+    } catch (error) {
+      console.log("Error fetching seller all products: " + error);
+      alert("Error retrieving products from the backend");
+    }
+  }
+
+  async function fetchSellerAllAffiliates() {
+    try {
+      const affiliates = await getAllAffiliatesBySellerId(user.email);
+      setSellerAllAffiliates(affiliates);
+      console.log(affiliates);
+    } catch (error) {
+      console.log("Error fetching seller all affiliates: " + error);
+      alert("Error retrieving affiliates from the backend");
+    }
+  }
+
+  useEffect(() => {
+    fetchSellerAllAffiliates();
+    fetchSellerAllProducts();
+  });
 
   return (
     <div className={styles.container}>
@@ -19,7 +54,28 @@ export default function SupplierManagement() {
       <main>
         <Sidebar user={user}></Sidebar>
         <div className={styles.contentContainer}>
-          <h1>MANAGEMENT</h1>
+          <h1 style={{ alignSelf: "flex-start", justifySelf: "flex-start" }}>
+            Your Products
+          </h1>
+          <div className={styles.carousell}>
+            {sellerAllProducts?.map((product) => (
+              <ProductCard product={product} />
+            ))}
+          </div>
+          <h1
+            style={{
+              alignSelf: "flex-start",
+              justifySelf: "flex-start",
+              marginTop: "1em",
+            }}
+          >
+            Your TikTok Affiliates
+          </h1>
+          <div className={styles.carousell}>
+            {sellerAllAffiliates?.map((affiliate) => (
+              <UserMiniCard user={affiliate} />
+            ))}
+          </div>
         </div>
       </main>
 
