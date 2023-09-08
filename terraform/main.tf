d11198fa714ca9928e0914cbf69ed0ffe4b6194d
DESKTOP-RLI54N2\Yu_Hao <yuhaochua99@gmail.com>
2023-09-08 17:47:27 +0800

    change image bucket public policy to false

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,10 +6,10 @@ resource "aws_s3_bucket" "images_bucket" {
 
 resource "aws_s3_bucket_public_access_block" "images_bucket_public_access_block" {
   bucket                  = var.images_bucket_name
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 # lambda bucket
 resource "aws_s3_bucket" "lambda_bucket" {
