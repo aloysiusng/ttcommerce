23d1d072caf8c487f2d69c79bcbefc1fdf3640cf
DESKTOP-RLI54N2\Yu_Hao <yuhaochua99@gmail.com>
2023-09-09 17:03:29 +0800

    Supplier analytics page

--- frontend/tt-hackathon-fe/pages/supplier-analytics.js
+++ frontend/tt-hackathon-fe/pages/supplier-analytics.js
@@ -1,12 +1,73 @@
 import Head from "next/head";
 import styles from "../styles/SupplierAnalytics.module.css";
-import { useContext, useState } from "react";
+import { useEffect, useContext, useState } from "react";
 import Sidebar from "../components/sidebar";
 import Navbar from "../components/Navbar";
 import { UserContext } from "./_app";
+import SellerMiniCard from "../components/TopSellerCard";
+import {
+  getTiktokerById,
+  getSupplierById,
+} from "../utils/topseller-service";
 
 export default function SupplierManagement() {
   const { user, setUser } = useContext(UserContext);
+  const [supplier, setSupplier] = useState([]);
+  const [supplierId, setSupplierId] = useState("7b0febb8-d61d-4ff6-be7c-120beb7ea691")
+  const [topSellers, setTopSellers] = useState([]);
+  const [tiktokers, setTiktokers] = useState([]);
+
+  async function fetchSupplier() {
+    try {
+      const current_supplier = await getSupplierById(supplierId);
+      // getSupplierById(supplierId).then((res) => setSupplier(res));
+      setSupplier(current_supplier);
+      console.log("supplier is " + supplier);
+      setTopSellers(supplier.tiktokers_sales)
+    } catch (error) {
+      console.log("Error fetching supplier: " + error);
+      alert("Error retrieving supplier from the backend");
+    }
+  }
+
+  async function fetchTopSellers() {
+    console.log(supplier.tiktokers_sales)
+    const sellers = Object.entries(supplier.tiktokers_sales);
+    sellers.sort((x, y) => x[1] - y[1]);
+    console.log(JSON.stringify(sellers));
+
+    for (let seller in sellers) {
+      try {
+        const tiktoker = await getTiktokerById(seller[0]);
+        console.log("tiktoker is " + tiktoker)
+        tiktokers.push(tiktoker);
+        setTiktokers(tiktokers);
+        console.log(tiktokers);
+      } catch (error) {
+        console.log("Error fetching tiktoker: " + error);
+        alert("Error retrieving tiktoker from the backend");
+      }
+    }
+  }
+
+  async function fetchTiktokers() {
+    
+    try {
+      const tiktoker = await getTiktokerById(user.email);
+      tiktokers.push(tiktoker)
+      setTiktokers(tiktokers);
+      console.log(tiktokers)
+    } catch (error) {
+      console.log("Error fetching tiktoker: " + error);
+      alert("Error retrieving tiktoker from the backend");
+    }
+  }
+
+  useEffect(() => {
+    fetchSupplier();
+    fetchTopSellers();
+    // fetchTiktokers();
+  }, [supplierId]);
 
   return (
     <div className={styles.container}>
@@ -24,6 +85,11 @@ export default function SupplierManagement() {
             20
           </div>
           <h1 className={styles.sectionTitle}>Top Selling Affiliates</h1>
+          <div className={styles.carousell}>
+            {tiktokers?.map((affiliate) => (
+              <SellerMiniCard seller={affiliate} />
+            ))}
+          </div>
           
         </div>
       </main>
---
+++ frontend/tt-hackathon-fe/utils/topseller-service.js
@@ -0,0 +1,41 @@
+import suppliers from "../mock/mock-supplier.json";
+import tiktokers from "../mock/mock-tiktokers.json";
+
+/* API LIST
+1) get_tiktoker_by_id
+2) get_supplier_by_id
+*/
+
+const getTiktokerById = (tiktokerId) => {
+  //MOCK
+  const tiktoker = tiktokers;
+  return new Promise((resolve, reject) => {
+    resolve(tiktoker);
+  });
+};
+
+const getSupplierById = async (supplierId) => {
+  //MOCK
+  const requestBody = { "supplier_id": supplierId }
+//   const supplier = ;
+//   return new Promise((resolve, reject) => {
+//     resolve(supplier);
+//   });
+
+  var url = `https://iytttt1316.execute-api.ap-southeast-1.amazonaws.com/api/get_supplier_by_id?supplier_id=${supplierId}`;
+  try {
+    const response = await fetch(url);
+    console.log(response)
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return new Promise((resolve, reject) => {
+        resolve(response.json());
+      });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export { getTiktokerById, getSupplierById };
